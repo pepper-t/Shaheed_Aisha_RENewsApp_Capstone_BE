1bbab66431e108023bac7e0df7686b861d794264
Aisha Shaheed <ps.ashaheed@outlook.com>
2026-03-10 00:17:33 -0400

    added file to models folder

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,1 +1,17 @@
 
+
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, 
+    required: true },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  
+});
+
+export default mongoose.model("Users", userSchema);
+
