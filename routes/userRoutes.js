977728636aba495124e1fe3bb711f492a88fd341
Aisha Shaheed <ps.ashaheed@outlook.com>
2026-03-10 00:35:29 -0400

    uploaded userRoutes from routes folder

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,1 +1,67 @@
 
+import express from "express";
+import User from "../models/UserSchema.js";
+import data from "../utilities/data.js";
+
+const router = express.Router();
+
+// router.get("/seed", async (req, res) => {
+//   try {
+//     await Character.deleteMany({});
+//     await Character.create(data);
+
+//     res.send("Seeded Database");
+//   } catch (error) {
+//     console.error(error.message);
+//     res.send("Seed failed");
+//   }
+// });
+
+// Create
+router
+  .route("/")
+  .post(async (req, res) => {
+    let newUser = await User.insertOne(req.body);
+
+    res.json(newUser);
+  })
+  //  Read - Show All
+  .get(async (_req, res) => {
+    let allUsers = await User.find({});
+
+    res.json(allUsers);
+  });
+
+// Update
+router
+  .route("/:id")
+  .put(async (req, res) => {
+    let updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true, runValidators: true },
+    );
+
+    if (!updatedUser) return res.status(404).json({ error: "User Not Found" });
+
+    res.json(updatedUser);
+  })
+  // Delete
+  .delete(async (req, res) => {
+    let deletedUser = await User.findByIdAndDelete(req.params.id);
+
+    if (!deletedUser) return res.status(404).json({ error: "User Not Found" });
+
+    res.json(deletedUser);
+  })
+
+  // Show One
+  .get(async (req, res) => {
+    let user = await User.findById(req.params.id);
+
+    if (!user) return res.status(404).json({ error: "User Not Found" });
+
+    res.json(user);
+  });
+
+export default router;
