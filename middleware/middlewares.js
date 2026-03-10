22d4c2595308c74102db2ad38dd9ce1afa21f7c7
Aisha Shaheed <ps.ashaheed@outlook.com>
2026-03-10 00:15:12 -0400

    added file from middleware folder

--- middleware/middlewares.js
+++ middleware/middlewares.js
@@ -1,1 +1,18 @@
 
+
+
+
+
+export function logReq(req, _res, next) {
+  console.log(
+    `${req.method} -- ${req.url} -- ${new Date().toLocaleTimeString()}`,
+  );
+
+  if (req.body) console.table(req.body);
+
+  next();
+}
+
+export function globalErr(err, _req, res, _next) {
+  res.status(err.status || 500).json({ error: `❌ Error: ${err.message}` });
+}
